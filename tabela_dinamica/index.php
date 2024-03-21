6e4eba52863b44a56e296f08ecaca9dd343068a9
Juliana <160780965+arsjuju@users.noreply.github.com>
2024-03-21 16:36:25 -0300

    Add files via upload

---
+++ tabela_dinamica/index.php
@@ -0,0 +1,21 @@
+<html>
+
+<head>
+</head>
+
+<body>
+	<h1> TABELA DINÂMICA </h1>
+
+	<form method="get" action="monta_tabela.php">
+		<p>
+			Tabuada do:
+			<input type="text" name="num">
+		</p>
+
+		<p>
+			<input type="submit" value="Montar tabela">
+		</p>
+	</form>
+</body>
+
+</html>
